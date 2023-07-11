69d7da034baa2851966d9b9e25299ff12c09fc2f
Aamr-mohamed <110783762+Aamr-mohamed@users.noreply.github.com>
2023-07-11 07:57:29 +0300

    added few features to the login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import LoginForm from "./loginform/app";
+import LoginForm from "./loginform/login";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="page">
-      <LoginForm />
+    <div
+      className="page"
+      style={{
+        backgroundImage: `url("https://img.freepik.com/free-vector/technology-background_23-2148119855.jpg?w=1380&t=st=1688998470~exp=1688999070~hmac=957eb0c4621e85c9ae1f4b85781a4407d450d2f59eb8badfdce7338ac38423ae")`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <BrowserRouter>
+        <LoginForm />
+      </BrowserRouter>
     </div>
   );
 }
